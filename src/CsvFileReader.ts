d311f5b98ead91f42f0eadd5b5829d40e17b65ee
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-07-11 07:46:20 +0900

    refactor: use interface

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -4,25 +4,17 @@ import {MatchResult} from "./MatchResult";
 
 type MatchData = [Date, string, string, number, number, MatchResult, string]
 
-export abstract class CsvFileReader {
-    private _data: MatchData[] = [];
+export class CsvFileReader {
+    data: string[][] = [];
 
     constructor(private _filename: string) {
     }
 
     read(): void {
-        this._data = fs.readFileSync(this._filename, {encoding: 'utf-8'})
+        this.data = fs.readFileSync(this._filename, {encoding: 'utf-8'})
             .split('\n')
             .map((row: string): string[] => {
                 return row.split(',');
-            })
-            .map(this.mapRow);
-
+            });
     }
-
-    get data(): MatchData[] {
-        return this._data;
-    }
-
-    abstract mapRow(row: string[]): MatchData;
 }
